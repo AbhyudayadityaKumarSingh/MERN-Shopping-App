ebf812aed3888e757e36be37c4a55ac36c9a39e8
AbhyudayadityaKumarSingh <abhyudayaditya.219302471@muj.manipal.edu>
2024-07-15 14:23:47 +0530

    made changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,7 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from 'redux-thunk'; // middleware
+import { legacy_createStore as createStore} from 'redux' ;
+import {  combineReducers, applyMiddleware } from "redux";
+
+import { thunk } from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension'; // devtools
 
 const reducer = combineReducers({}); // combine reducers
